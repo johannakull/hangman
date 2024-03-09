72756205a334131cfc9b8b0eb8931bfceffeda00
johannakull <johanna.kull@live.de>
2024-03-09 19:37:59 +0000

    add check if letter has already been guessed

--- code/main.py
+++ code/main.py
@@ -8,6 +8,7 @@ word_length = len(random_word)
 lives = 7
 
 display = []
+guessed_letters = []
 
 print("\nWELCOME TO HANGMAN\n")
 print(f"Try to guess the following word! You have {lives} lives.\n")
@@ -20,26 +21,31 @@ print()
 
 while "_" in display and lives > 0:
     guess = input("\nGuess a letter: ").upper()
-    
-    if guess in random_word:
-        for position in range(word_length):
-            letter = random_word[position]
-            if letter == guess:
-                display[position] = guess
-        print("\nCorrect!\n")
-    else:
-        lives -= 1
-        if lives > 1:
-            print(f"\nSorry, {guess} is incorrect - you lose a life. You now have {lives} lives left.\n")
-        elif lives == 1:
-            print(f"\nSorry, {guess} is incorrect - you lose a life. You now have {lives} life left.\n")
-        else:
-            print()
 
-        print(hangman_art.stages[lives])
+    if guess in guessed_letters:
+        print(f"You already guessed the letter {guess}. Try a different one.")
+    else:
+        guessed_letters += guess
+
+        if guess in random_word:
+            for position in range(word_length):
+                letter = random_word[position]
+                if letter == guess:
+                    display[position] = guess
+            print("\nCorrect!\n")
+        else:
+            lives -= 1
+            if lives > 1:
+                print(f"\nSorry, {guess} is incorrect - you lose a life. You now have {lives} lives left.\n")
+            elif lives == 1:
+                print(f"\nSorry, {guess} is incorrect - you lose a life. You now have {lives} life left.\n")
+            else:
+                print()
+
+            print(hangman_art.stages[lives])
     
-    if lives > 0:
-        print(*display, sep=" ")
+        if lives > 0:
+            print(*display, sep=" ")
 
 if lives == 0:
     print(f"You lost all your lives - Game Over! The word was {random_word}.")

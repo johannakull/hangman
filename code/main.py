f027c5b1fd0605fa8800f9143bc4cd4c58053c0a
johannakull <johanna.kull@live.de>
2024-03-09 17:17:51 +0000

    add lives and win/lose messages

--- code/main.py
+++ code/main.py
@@ -2,6 +2,8 @@ import random
 
 word_list = ["automobile", "banana", "caramel"]
 
+lives = 6
+
 random_word = random.choice(word_list)
 word_length = len(random_word)
 
@@ -15,14 +17,24 @@ for _ in random_word:
 print("Here's your word!")
 print(display)
 
-while "_" in display:
+while "_" in display and lives > 0:
     guess = input("Guess a letter: ").lower()
     
-    for position in range(word_length):
-        letter = random_word[position]
-        if letter == guess:
-            display[position] = guess
+    if guess in random_word:
+        for position in range(word_length):
+            letter = random_word[position]
+            if letter == guess:
+                display[position] = guess
+    else:
+        lives -= 1
+        if lives > 0:
+            print(f"Wrong! You lost a life. You now have {lives} lives left.")
     
     print(display)
+
+if lives == 0:
+    print("You lost the game. Try again.")
+else:
+    print("Congratulations! You win.")
         
 

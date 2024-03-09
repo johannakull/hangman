5fd11c91a3f230a1084236396c49233e66c4439a
johannakull <johanna.kull@live.de>
2024-03-09 17:37:40 +0000

    add some user interaction & formatting

--- code/main.py
+++ code/main.py
@@ -1,40 +1,45 @@
 import random
 
 word_list = ["automobile", "banana", "caramel"]
+random_word = random.choice(word_list).upper()
+word_length = len(random_word)
 
 lives = 6
 
-random_word = random.choice(word_list)
-word_length = len(random_word)
-
-print(random_word) # for debugging
-
 display = []
+
+print("WELCOME TO HANGMAN\n")
+print("Try to guess the following word! You have 6 lives.\n")
 
 for _ in random_word:
     display += ("_")
 
-print("Here's your word!")
-print(display)
+print(*display, sep=" ")
+print()
 
 while "_" in display and lives > 0:
-    guess = input("Guess a letter: ").lower()
+    guess = input("\nGuess a letter: ").upper()
     
     if guess in random_word:
         for position in range(word_length):
             letter = random_word[position]
             if letter == guess:
                 display[position] = guess
+        print("\nCorrect!\n")
     else:
         lives -= 1
-        if lives > 0:
-            print(f"Wrong! You lost a life. You now have {lives} lives left.")
+        if lives > 1:
+            print(f"\nSorry, {guess} is incorrect - you lose a life. You now have {lives} lives left.\n")
+        elif lives == 1:
+            print(f"\nSorry, {guess} is incorrect - you lose a life. You now have {lives} life left.\n")
+        else:
+            print()
     
-    print(display)
+    print(*display, sep=" ")
 
 if lives == 0:
-    print("You lost the game. Try again.")
+    print("\nYou lost the game. Try again.")
 else:
-    print("Congratulations! You win.")
+    print("\nCongratulations! You win.")
         
 
